8f1931c56879d0e6d5ba929ec8b8a6cf835b6385
Duy Phan <hoangduy28101997@gmail.com>
2025-06-07 14:15:27 +0700

    first commit

---
+++ app/HelloWorldApi.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace HelloWorldApi.Tests
+{
+    public class HelloEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public HelloEndpointTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task HelloEndpoint_ReturnsHelloMessage()
+        {
+            var response = await _client.GetAsync("/hello");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal("Hello, CI/CD!", content);
+        }
+    }
+}
