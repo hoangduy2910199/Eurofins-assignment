94894be8f6d3e155fbd9ce9e07a6804b8507cddb
Duy Phan <hoangduy28101997@gmail.com>
2025-06-08 20:40:56 +0700

    Update - add some comment

--- app/HelloWorldApi/Program.cs
+++ app/HelloWorldApi/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipelines.
 app.MapGet("/", () => "Hello, Eurofins!");
 
 app.Run();
